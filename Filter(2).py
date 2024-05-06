a950d9e7a4dee9fa909bf4be2887011882d1b24f
Dxprit-hack <163235300+Dxprit-hack@users.noreply.github.com>
2024-05-06 19:26:47 +0330

    Create Filter(2).py

---
+++ Filter(2).py
@@ -0,0 +1,4 @@
+"https://fa.m.wikipedia.org/wiki/%DA%A9%D8%B4%D8%AA%D9%87%E2%80%8C%D8%B4%D8%AF%D9%86_%D9%85%D9%87%D8%B3%D8%A7_%D8%A7%D9%85%DB%8C%D9%86%DB%8C"
+"iran terrorist"
+"report account terrorist"
+"https://parsi.euronews.com/2023/09/13/mahsa-amini-city-of-saqez-one-year-going-on-funeral-ceremony-storytelling"
