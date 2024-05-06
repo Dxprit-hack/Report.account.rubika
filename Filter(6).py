76b8e834c727e5ddb87d53544ba54ecf42966261
Dxprit-hack <163235300+Dxprit-hack@users.noreply.github.com>
2024-05-06 19:36:35 +0330

    Create Filter(6).py

---
+++ Filter(6).py
@@ -0,0 +1,4 @@
+("Advertising and promoting the use of drugs psychotropic substances and cigarettes")
+("Publishing content containing incitement, ncitement, or invitation to violent and suicidal acts")
+("Content that incites, encourages, or invites to commit a crime")
+("Encouraging and encouraging the violation of intellectual property rights")
